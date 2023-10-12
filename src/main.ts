e6c0d12e59247b88393fd478c5512ffb8225718a
NanoB0tt <marianofassikoropecki@gmail.com>
2023-10-12 11:21:33 -0300

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  /* app.enableCors({ */
-  /*   origin: true, */
-  /*   allowedHeaders: ['Content-Type', 'Authorization'], */
-  /*   credentials: true, */
-  /* }); */
+  app.enableCors({
+    origin: "https://chat-ohthmw5qf-nanob0tts-projects.vercel.app",
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
